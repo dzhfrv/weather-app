55e77347221599fe7dd0a98f408503fd622f6c2f
dzhfrv <solomonickey@gmail.com>
2021-04-04 20:58:40 +0300

    provided README

--- README.md
+++ README.md
@@ -1,21 +1,42 @@
-# (WIP)weather-app
-
+# weather-app
+_Python3, Django, Django-rest-framework, Postgres_
 ###.env file is required
+_for receiving WEATHER_API_KEY visit: https://home.openweathermap.org/api_keys_
 ```dotenv
 SECRET_KEY = ''
 DEBUG = True
 WEATHER_API_KEY = ''
-DB_NAME = ''
+DB_NAME = 'Postgres DB Name'
 DB_USER = ''
 DB_PWD = ''
 ```
 
+### installation
+* setup PostgreSQL db
+  
+* create `.env` file in project root
+* `python3 -m venv env`
+* `.env/bin/activate`
+* `pip install -r requirements.txt`
+* `python manage.py migrate`
 ### request example
 
+`http://127.0.0.1:8000/weather-api/weather/`
 ```json
 {
     "search_type":"current",
-    "search_lat": 32.2134121,
+    "search_lat": 32.213412,
     "search_lon": 20.243213
 }
+```
+- __search_lat__ - lattitude
+- __search_lon__ - longitude
+- __search_type__ - detailing type
+  (accepts "current", "minute", "hourly" or "daily")
+
+```
+current: Current weather
+minute: Minute forecast for 1 hour
+hourly: Hourly forecast for 48 hours
+daily: Daily forecast for 7 days
 ```
--- apps/weather/tests.py
+++ apps/weather/tests.py
@@ -17,13 +17,12 @@ class TestWeatherEndpoint(APITestCase):
             "search_lon": 20.12341,
             "search_type": "current",
         }
-
-        self.invalid_lat_data = {
+        self.invalid_data = {
             "search_lat": "",
             "search_lon": 20.12341,
             "search_type": "current",
         }
-        self.no_type_data = {
+        self.no_requied_data = {
             "search_lat": 32.43231,
             "search_lon": 20.12341,
         }
@@ -40,12 +39,25 @@ class TestWeatherEndpoint(APITestCase):
         self.client.post(self.endpoint, self.valid_data)
         self.assertEqual(Weather.objects.count(), 1)
 
-    def test_endpoint_invalid_lat_data(self):
-        resp = self.client.post(self.endpoint, self.invalid_lat_data)
+    def test_endpoint_invalid_data(self):
+        resp = self.client.post(self.endpoint, self.invalid_data)
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(resp.status_text, "Bad Request")
+
+    def test_no_data(self):
+        resp = self.client.post(self.endpoint, {})
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(resp.status_text, "Bad Request")
 
     def test_endpoint_invalid_search_type_data(self):
-        resp = self.client.post(self.endpoint, self.no_type_data)
+        resp = self.client.post(self.endpoint, self.no_requied_data)
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(resp.status_text, "Bad Request")
+
+    def test_endpoint_methods_allowance(self):
+        resp = self.client.get(self.endpoint)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        resp = self.client.put(self.endpoint)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        resp = self.client.patch(self.endpoint)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
